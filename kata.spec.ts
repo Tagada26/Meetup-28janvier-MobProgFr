93b23488da2ee154aee2a9c822212c956d630615
Marjorie <94189700+Tagada26@users.noreply.github.com>
2025-01-28 21:26:37 +0100

    Update kata.spec.ts

--- kata.spec.ts
+++ kata.spec.ts
@@ -1,17 +1,55 @@
 import { describe, it, expect } from "vitest";
 
-const kata = () => {
-  return "Let's have fun with mob ";
+// The Kata says you should write a function to convert from normal numbers to Roman Numerals
+// 1	5	 10	 50	 100	500	1000
+// I	V	 X	 L	 C	  D	  M
+
+const convert = (number: number) => {
+  if (number > 5 && number < 9) {
+    const rest = number - 5;
+    return "V" + "I".repeat(rest);
+  }
+  if (number === 5) return "V";
+  if (number === 50) return "L";
+  return "I";
 };
 
-describe("Guess nearest primary colors", () => {
-  it("F00 nearest color is F00", () => {
-    //Given
+describe("Roman numerals", () => {
+  it("it should return 'V'", () => {
+    //Arrange
 
-    //When
-    const doKata = kata();
+    //Act
+    const doConvert = convert(5);
 
-    //Then
-    expect(doKata).toBeDefined();
+    //Assert
+    expect(doConvert).toBe("V");
+  });
+
+  it("it should return 'I'", () => {
+    // Given
+
+    // When
+    const doConvert = convert(1);
+
+    expect(doConvert).toBe("I");
+  });
+
+  it("it should return 'L'", () => {
+    // Given
+
+    // When
+    const doConvert = convert(50);
+
+    expect(doConvert).toBe("L");
+  });
+
+  it("it should return 'VI'", () => {
+    // Given
+
+    // When
+    const doConvert = convert(6);
+
+    expect(doConvert).toBe("VI");
   });
 });
+
